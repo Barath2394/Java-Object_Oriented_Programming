0d4015a1294a8753cf2232aed0d9ec30817433ef
Barath <barathbarth128@gmail.com>
2024-04-21 13:42:02 +0530

    OOP's

---
+++ Object_Oriented_Programming/PolyMorphism/Encapsulation/Bundling.java
@@ -0,0 +1,22 @@
+package Java.Object_Oriented_Programming.PolyMorphism.Encapsulation;
+
+class Info{
+    private String name;
+    private int roll;
+
+    public void setfun(String name,int roll){
+        this.name = name;
+        this.roll = roll;
+    }
+    public void getfun(){
+        System.out.println("Name : " + name);
+        System.out.println("Roll : " + roll);
+    }
+}
+public class Bundling {
+    public static void main(String[] args) {
+        Info obj = new Info();
+        obj.setfun("Barath",11);
+        obj.getfun();
+    }
+}
