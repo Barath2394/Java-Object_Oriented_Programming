d940bbbdd99edf7d542155f05a6965d7cf5b1ec1
Barath <barathbarth128@gmail.com>
2024-04-22 17:56:00 +0530

    Collections

---
+++ Object_Oriented_Programming/Collections/Hashset.java
@@ -0,0 +1,16 @@
+package Java-Object_Oriented_Programming.Object_Oriented_Programming.Collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Hashset {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("C");
+        set.add("C++");
+        set.add("Java");
+        set.add("Python");
+        set.add("JS");
+        System.out.println(set);
+    }
+}
